6ac1017c3f8c1386a547dd4f19dc108609698bc4
powercore649 <126120353+powercore649@users.noreply.github.com>
2024-09-07 20:34:07 -0400

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,9 +2,9 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 2024-09-07 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Every discord server can use the circle protect bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
 <p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
